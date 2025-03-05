e1db3419adb257d1648e288d64aababb7881a7c0
LucasGuliano <52748594+LucasGuliano@users.noreply.github.com>
2025-03-05 15:13:52 -0500

    Update motor_oscillation.py

--- motor_oscillation.py
+++ motor_oscillation.py
@@ -1,3 +1,7 @@
+##################
+# Designed to start at origin, move forward, and then backwards
+##################
+
 import serial_util_v3 as serial_util
 import time
 
@@ -32,6 +36,10 @@ if port_connect==True:
     #Start with testing true to give the user the options
     testing = True
 
+###########################################
+#########    TESTING LOOP #################
+###########################################
+
     while testing == True:     
         #Allow user to either perform a test run or start oscillation
         print("Peform test or start oscillation?")
@@ -41,7 +49,7 @@ if port_connect==True:
             testing = True
             print('Performing test run')
             #Forwards and wait until finished
-            serial_util._set_position(motor_port, stage='X', dx=OPD)
+            serial_util._set_position(motor_port, stage='X', dx=OPD/2)
             print('Moving Forward')
             while serial_util._check_connection(motor_port) == False:
                 time.sleep(0.1)
@@ -51,20 +59,42 @@ if port_connect==True:
             print('Moving Backwards')
             while serial_util._check_connection(motor_port) == False:
                 time.sleep(0.1)
+                
+            #Return to origin
+            serial_util._move_to_origin(motor_port)
+            print('Returning to origin')
+            while serial_util._check_connection(motor_port) == False:
+                time.sleep(0.1)
 
-        #When testing not set, enter endless loop of back and forth    
+###########################################
+#########   MAIN OSCILLATION LOOP #########
+###########################################
+
+        #When testing not set, enter endless loop of back and forth   
+        #Starts at origin, moves forward half the OPD, moves backwards full OPD, then forward full OPD....
         if test_input != 't':
             testing = False
             print('Starting oscillation')
             
         while testing == False:
+            
+            #First step move forward hald the OPD from origin
+            if direction_flag == 0:
+                serial_util._set_position(motor_port, stage='X', dx=+OPD/2)
+                while serial_util._check_connection(motor_port) == False:
+                    time.sleep(0.1)
+            
+            #Add to the direction flag to alternate the direction of movement
             direction_flag += 1
-            if direction_flag % 2 == 0:
+            
+            # Move backwards if the flag is odd, should happen first after the first half step forward
+            if direction_flag % 2 == 1:
                 serial_util._set_position(motor_port, stage='X', dx=-OPD)
                 while serial_util._check_connection(motor_port) == False:
                     time.sleep(0.1)
 
-            if direction_flag % 2 == 1:
+            # Move forwards if the flag is even
+            if direction_flag % 2 == 0:
                 serial_util._set_position(motor_port, stage='X', dx=+OPD)
                 while serial_util._check_connection(motor_port) == False:
                     time.sleep(0.1)
